f215bc6c40fa54f56a88d2c728fb7f2d3e4db4db
Cynthia Ma <cynthia.ma.emily@gmail.com>
2017-11-26 23:21:10 -0800

    Prepare for heroku push

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
+// import { composeWithDevTools } from 'redux-devtools-extension';
 
 import RootReducer from '../reducers/root_reducer';
 
